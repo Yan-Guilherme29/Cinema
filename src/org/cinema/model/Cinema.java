94aa3e29ff7e2f2941e1cd5680defcadf0fa7520
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2025-09-08 20:59:49 -0300

    Sétimo commit. Classe Cinema adicionada.

---
+++ src/org/cinema/model/Cinema.java
@@ -0,0 +1,31 @@
+package org.cinema.model;
+
+public class Cinema {
+    private String nome;
+    private String endereco;
+
+
+    public Cinema() {
+    }
+
+    public Cinema(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
